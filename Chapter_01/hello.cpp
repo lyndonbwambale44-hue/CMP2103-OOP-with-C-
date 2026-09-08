f8ef2b44eefdd01f50cbb80d927e1ebd112a38a1
Lyndon <lyndonbwambale44@gmail.com>
2026-09-08 22:47:43 +0300

    Ascending sequence

--- Chapter_01/hello.cpp
+++ Chapter_01/hello.cpp
@@ -1,12 +1,23 @@
 // This program outputs the message "Hello, World!" to the monitor
 
+<<<<<<< HEAD
 #include <iostream>
 using namespace std; // gain access to the C++ standard library
+=======
+import std; // gain access to the C++ standard library
+
+>>>>>>> 022cb7432f817cd90c9b1e703f19caa0c197c748
 int main() // C++ programs start by executing the function main
 {
     std::cout << "Hello, World!\n"; // output "Hello, Wor ld!"
     return 0; // Means the program executed successfully. It can be omitted.
 }
+
+
+
+
+
+
 
 
 // If you don’t have an up-to-date C++ implementation that supports module std,

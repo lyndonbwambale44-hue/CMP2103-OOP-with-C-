46ce012eca420bf67672b38f5dce7f7b67e18901
Lyndon <lyndonbwambale44@gmail.com>
2026-09-09 13:54:56 +0300

    JOURNEY

--- Chapter_01/journey.cpp
+++ Chapter_01/journey.cpp
@@ -1,4 +1,17 @@
 // Write a description of how to get from the front door of your dorm room, apartment, house, whatever, to the door of your classroom (assuming you are attending some school; if you are
 // not, pick another target). Have a friend try to follow the instructions and annotate them with improvements as he or she goes along. To keep friends, it may be a good idea to ‘‘field test’’
 // those instructions yourself before giving them to a friend.
-
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "To get from my dorm room to the classroom, follow these steps:\n";
+    cout << "1. Exit the dorm room and turn right, and then left.\n";
+    cout << "2. Walk straight up towards Mitchell Hall until you reach the junction, and then turn right.\n";
+    cout << "3. Walkstraight for a few meters and turn left towards UBA bank, continue straight through to ivory tower and turn right.\n";
+    cout << "4. Walk straight for about 200 meters until you reach the round about.\n";
+    cout << "5. Continue walking straight for another 400 meters.\n";
+    cout << "6. Turn left, and you'll see College of Engineering, Design Art and Technology.\n";
+    cout << "7. The classroom is room M2, in the basement.\n";
+    
+    return 0;
+}

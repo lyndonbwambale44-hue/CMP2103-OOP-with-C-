722a2b9a2e89a864a5610a80f46269c4ed31adfc
Lyndon <lyndonbwambale44@gmail.com>
2026-09-09 13:35:31 +0300

    Alphabetical order

--- Chapter_02/alphabetical_order.cpp
+++ Chapter_02/alphabetical_order.cpp
@@ -1,3 +1,27 @@
 // Rewrite the ascending.cpp program to process stings instead.
 // So, if the user enters the values Electrical Eng., Computer Eng., Biomedical Eng.,
 // the output should be Biomedical Eng., Computer Eng., Electrical Eng..
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main() {
+    string val1, val2, val3;
+    cout << "Enter three words: ";
+    getline(cin, val1);
+    getline(cin, val2);
+    getline(cin, val3);
+
+    // Simple sorting logic for three values
+    if (val1 > val2) {
+        swap(val1, val2);
+    }
+    if (val2 > val3) {
+        swap(val2, val3);
+    }
+    if (val1 > val2) {
+        swap(val1, val2);
+    }
+
+    cout << val1 << ", " << val2 << ", " << val3 << endl;
+    return 0;
+}
